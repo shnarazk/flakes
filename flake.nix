c19f62d49a986d2991c4ab1e9bb9a606a0fc3b88
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-11-07 05:57:43 +0900

    add sat-bench and splr

--- flake.nix
+++ flake.nix
@@ -2,15 +2,19 @@
   description = "Piling up my flakes";
   inputs = {
     flake-utils.url = github:numtide/flake-utils;
-    emacs-head.url = github:shnarazk/flakes?dir=emacs-head;
-    kissat.url = github:shnarazk/flakes?dir=kissat;
+    emacs-head.url  = github:shnarazk/flakes?dir=emacs-head;
+    kissat.url      = github:shnarazk/flakes?dir=kissat;
+    sat-bench.url   = github:shnarazk/SAT-bench;
+    splr.url        = github:shnarazk/splr;
   };
-  outputs = { self, flake-utils, emacs-head, kissat }:
+  outputs = { self, flake-utils, emacs-head, kissat, sat-bench, splr }:
     flake-utils.lib.eachDefaultSystem (system:
       {
         packages = {
           emacs-head = emacs-head.defaultPackage.${system};
-          kissat = kissat.defaultPackage.${system};
+          kissat     = kissat.defaultPackage.${system};
+          sat-bench  = sat-bench.defaultPackage.${system};
+          splr       = splr.defaultPackage.${system};
         };
       }
     );
