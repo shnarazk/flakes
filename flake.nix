b356e275ca7509c360d89f852240e3c4551b6005
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-11-06 10:48:58 +0900

    simplify

--- flake.nix
+++ flake.nix
@@ -1,13 +1,12 @@
 {
-  description = "piling my flakes";
+  description = "piling up my flakes";
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
   inputs.flake-utils.url = github:numtide/flake-utils;
   inputs.emacs-head.url = "./emacs-head";
   inputs.kissat.url = "./kissat";
   outputs = { self, nixpkgs, flake-utils, emacs-head, kissat }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
-      rec {
+      {
         packages = {
           emacs-head = emacs-head.defaultPackage.${system};
           kissat = kissat.defaultPackage.${system};
