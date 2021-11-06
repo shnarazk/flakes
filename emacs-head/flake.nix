f481153eba071bf8d612ce4129c4290c19477097
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-11-06 09:38:32 +0900

    revise and add kissat

--- emacs-head/flake.nix
+++ emacs-head/flake.nix
@@ -1,39 +1,37 @@
 {
   description = "Emacs Head, the unreleased 28.0";
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
-  outputs = { self, nixpkgs }: {
-    defualtPackage.x86_64-darwin =
-      with import nixpkgs { system = "x68_64-darwin"; };
-      nixpkgs.emacs.overrideAttrs (attrs: rec {
-        name = "emacs-head";
-        # name = "emacs-head-${version}";
-        version = "28.0.60";
-        src = nixpkgs.fetchFromGitHub {
-          owner = "emacs-mirror";
-          repo = "emacs";
-          rev = "e3c3c78365c6430b0e9015c1a3701518de4680cd";
-          #sha256 = "1111111111111111111111111111111111111111111111111111";
-          sha256 = "sha256-Lrg1d4QwjPohuR2q3Pem9bZ1X/zTUGfA3Y6CGn2e0qU=";
-        };
-        patches = [];
-        preConfigure = ''
-      ./autogen.sh
-          '' + ''
-      substituteInPlace lisp/international/mule-cmds.el \
-        --replace /usr/share/locale ${nixpkgs.gettext}/share/locale
-
-      for makefile_in in $(find . -name Makefile.in -print); do
-          substituteInPlace $makefile_in --replace /bin/pwd pwd
-      done
-    '';
-        nativeBuildInputs = [ nixpkgs.pkgconfig ]
-                            ++ [ nixpkgs.autoconf nixpkgs.automake nixpkgs.texinfo ];
-        buildInputs = nixpkgs.emacs.buildInputs
-                      ++ [ nixpkgs.autoconf nixpkgs.automake nixpkgs.texinfo ]
-	                    ++ [ AppKit Carbon Cocoa IOKit OSAKit Quartz QuartzCore WebKit ImageCaptureCore GSS ImageIO ];
-        inherit (nixpkgs.darwin.apple_sdk.frameworks)
-          AppKit Carbon Cocoa IOKit OSAKit Quartz QuartzCore WebKit
-          ImageCaptureCore GSS ImageIO;
-      });
-  };
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      defaultPackage =
+        with import nixpkgs { system = "${system}"; };
+        emacs27.overrideAttrs (attrs: rec {
+          name = "emacs-head-${version}";
+          pname = "emacs-head";
+          version = "28.0.60";
+          src = fetchFromGitHub {
+            owner = "emacs-mirror";
+            repo = "emacs";
+            rev = "e3c3c78365c6430b0e9015c1a3701518de4680cd";
+            #sha256 = "1111111111111111111111111111111111111111111111111111";
+            sha256 = "sha256-VfX3A8/B+OTCAi76JmjlNcGHcNaBuKVSNADNDNzQW9E=";
+          };
+          patches = [];
+          preConfigure = ''
+          ./autogen.sh
+              '' + ''
+          substituteInPlace lisp/international/mule-cmds.el \
+            --replace /usr/share/locale ${gettext}/share/locale
+    
+          for makefile_in in $(find . -name Makefile.in -print); do
+              substituteInPlace $makefile_in --replace /bin/pwd pwd
+          done
+        '';
+          nativeBuildInputs = [ pkgconfig autoconf automake texinfo ];
+          buildInputs = emacs27.buildInputs
+                        ++ [ autoconf automake texinfo ]
+                        ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.AppKit darwin.apple_sdk.frameworks.Carbon darwin.apple_sdk.frameworks.Cocoa darwin.apple_sdk.frameworks.IOKit darwin.apple_sdk.frameworks.OSAKit darwin.apple_sdk.frameworks.Quartz darwin.apple_sdk.frameworks.QuartzCore darwin.apple_sdk.frameworks.WebKit darwin.apple_sdk.frameworks.ImageCaptureCore darwin.apple_sdk.frameworks.GSS darwin.apple_sdk.frameworks.ImageIO ];
+        });
+    });
 }
--- flake.nix
+++ flake.nix
@@ -1,42 +1,13 @@
 {
-  description = "Emacs Head, the unreleased 28.0";
+  description = "piling my flakes";
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
-  outputs = { self, nixpkgs }: {
-    defaultPackage.x86_64-darwin =
-      with import nixpkgs { system = "x86_64-darwin"; };
-      emacs27.overrideAttrs (attrs: rec {
-        # stdenv.mkDerivation {
-        name = "emacs-head";
-        pname = "emacs-head";
-        # name = "emacs-head-${version}";
-        version = "28.0.60";
-        src = fetchFromGitHub {
-          owner = "emacs-mirror";
-          repo = "emacs";
-          rev = "e3c3c78365c6430b0e9015c1a3701518de4680cd";
-          #sha256 = "1111111111111111111111111111111111111111111111111111";
-          sha256 = "sha256-VfX3A8/B+OTCAi76JmjlNcGHcNaBuKVSNADNDNzQW9E=";
-        };
-        patches = [];
-        preConfigure = ''
-          ./autogen.sh
-              '' + ''
-          substituteInPlace lisp/international/mule-cmds.el \
-            --replace /usr/share/locale ${gettext}/share/locale
-    
-          for makefile_in in $(find . -name Makefile.in -print); do
-              substituteInPlace $makefile_in --replace /bin/pwd pwd
-          done
-        '';
-        nativeBuildInputs = [ pkgconfig autoconf automake texinfo ];
-        buildInputs = emacs27.buildInputs
-                      ++ [ autoconf automake texinfo ]
-	                    ++ [ darwin.apple_sdk.frameworks.AppKit darwin.apple_sdk.frameworks.Carbon darwin.apple_sdk.frameworks.Cocoa darwin.apple_sdk.frameworks.IOKit darwin.apple_sdk.frameworks.OSAKit darwin.apple_sdk.frameworks.Quartz darwin.apple_sdk.frameworks.QuartzCore darwin.apple_sdk.frameworks.WebKit darwin.apple_sdk.frameworks.ImageCaptureCore darwin.apple_sdk.frameworks.GSS darwin.apple_sdk.frameworks.ImageIO ];
-        # installTargets = emacs27.installTargets;
-        # postInstall = emacs27.postInstall;
-        # inherit (nixpkgs.darwin.apple_sdk.frameworks)
-        #   AppKit Carbon Cocoa IOKit OSAKit Quartz QuartzCore WebKit
-        #   ImageCaptureCore GSS ImageIO;
-      });
-  };
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.emacs-head.url = "./emacs-head";
+  inputs.kissat.url = "./kissat";
+  outputs = { self, nixpkgs, flake-utils, emacs-head, kissat }:
+    flake-utils.lib.eachDefaultSystem (system: 
+      {
+        packages = { inherit emacs-head kissat; };
+      }
+    );
 }
