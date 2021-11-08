47f4ba75dc936c12207040f3bd2e432c92330d0c
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-11-08 20:44:50 +0900

    fix error

--- emacs-head/flake.nix
+++ emacs-head/flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
     flake-utils.url = github:numtide/flake-utils;
-  }
+  };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: {
       defaultPackage =
