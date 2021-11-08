f063552a66dec0429c1a788a445ac4b0a72aad6b
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-11-08 20:39:00 +0900

    fix error

--- cadical/flake.nix
+++ cadical/flake.nix
@@ -4,7 +4,7 @@
     nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
     flake-utils.url = github:numtide/flake-utils;
   };
-  outputs = { self, nixpkgs, flake-utils }: {
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: {
       defaultPackage =
         with import nixpkgs { system = "${system}"; };
@@ -45,4 +45,6 @@
             license = licenses.mit;
             homepage = "http://fmv.jku.at/cadical";
           };
- }
+        };
+    });
+}
