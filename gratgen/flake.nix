eb6fc31b1377f2303e8e2e5099bbf9ed33ffc51d
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-11-10 21:01:15 +0900

    modified:   gratgen/flake.nix

--- gratgen/flake.nix
+++ gratgen/flake.nix
@@ -13,13 +13,8 @@
           version = "2021-11-10";
           src = fetchTarball {
             url = "https://www21.in.tum.de/~lammich/grat/gratgen.tgz";
-            sha256 = "sha256-8LzV16WZUt581CNJ7qr8ZmGpASFdBE+6T8XStUnpK7Y=";
+            sha256 = "sha256:1qwnnlldm5phmhs8ay7g42qnsfp2wk4rkkhrxvwk7hq2r8ma88y9";
           };
-          #   owner = "arminbiere";
-          #   repo = "kissat";
-          #   rev = "abfa45fb782fa3b7c6e2eb6b939febe74d7270b7";
-          #   sha256 = "06pbmkjxgf2idhsrd1yzvbxr2wf8l06pjb38bzbygm6n9ami89b8";
-          # };
           # outputs = [ "out" "dev" "lib" ];
           # checkInputs = [ ];
           nativeBuildInput = [ cmake ];
@@ -30,16 +25,12 @@
           setOutputFlags = false;
           installPhase = ''
             runHook preInstall
-            install -Dm0755 build/kissat "$out/bin/kissat"
-            install -Dm0644 src/kissat.h "$dev/include/kissat.h"
-            install -Dm0644 build/libkissat.a "$lib/lib/libkissat.a"
-            mkdir -p "$out/share/doc/kissat/"
-            install -Dm0644 {LICEN?E,README*,VERSION} "$out/share/doc/kissat/"
+            install -Dm0755 gratgen "$out/bin/gratgen"
             runHook postInstall
           '';
 
           meta = with lib; {
-            description = "";
+            description = "A generator in Efficient Formally Verified SAT Solver Certification Toolchain";
             maintainers = with maintainers; [ shnarazk ];
             platforms = platforms.unix;
             license = licenses.mit;
