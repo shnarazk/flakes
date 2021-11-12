8befcf7b5a2b3799e49f7e25df38d1302eca4ac8
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-11-13 01:16:22 +0900

    update emacs-head

--- emacs-head/flake.nix
+++ emacs-head/flake.nix
@@ -6,38 +6,46 @@
   };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: {
+      overlay = self: prev: {
+        emacs-head = prev.emacs-head.override {
+          nativeComp = true;
+        };
+      };
       defaultPackage =
         with import nixpkgs { inherit system; };
-        emacs27 // rec {
-        # emacs27.overrideAttrs (attrs: rec {
-          nativeComp = true;
-          name = "emacs-head-${version}";
-          pname = "emacs-head";
-          version = "28.0.60-20211112-3";
-          src = fetchFromGitHub {
-            owner = "emacs-mirror";
-            repo = "emacs";
-            rev = "0d0125daaeb77af5aa6091059ff6d0c1ce9f6cff";
-            sha256 = "1q3gpa03pwgl1ry51m1ldxryna2g0xmy62vxnlxrkiqdkl7ikc3v";
+        let
+          emacsNative = emacs27.override {
+            nativeComp = true;
           };
-          patches = [];
-          preConfigure = ''
-          ./autogen.sh
-              '' + ''
-          substituteInPlace lisp/international/mule-cmds.el \
-            --replace /usr/share/locale ${gettext}/share/locale
-    
-          for makefile_in in $(find . -name Makefile.in -print); do
-              substituteInPlace $makefile_in --replace /bin/pwd pwd
-          done
-        '';
-          buildInputs = emacs27.buildInputs
-                        ++ [ autoconf automake texinfo gcc libgccjit ]
-                        ++ lib.optionals stdenv.isDarwin (
-                          with darwin.apple_sdk.frameworks; [
-                            AppKit Carbon Cocoa GSS ImageIO ImageCaptureCore
-                            IOKit OSAKit Quartz QuartzCore WebKit
-                          ]);
-        };
+        in
+          emacsNative.overrideAttrs (attrs: rec {
+            name = "emacs-head-${version}";
+            pname = "emacs-head";
+            version = "28.0.60-20211112-7";
+            src = fetchFromGitHub {
+              owner = "emacs-mirror";
+              repo = "emacs";
+              rev = "0d0125daaeb77af5aa6091059ff6d0c1ce9f6cff";
+              sha256 = "1q3gpa03pwgl1ry51m1ldxryna2g0xmy62vxnlxrkiqdkl7ikc3v";
+            };
+            patches = [];
+            preConfigure = ''
+            ./autogen.sh
+                '' + ''
+            substituteInPlace lisp/international/mule-cmds.el \
+              --replace /usr/share/locale ${gettext}/share/locale
+                 for makefile_in in $(find . -name Makefile.in -print); do
+                substituteInPlace $makefile_in --replace /bin/pwd pwd
+            done
+          '';
+            configureFlags = emacs27.configureFlags ++ ["--with-native-compilation"];
+            buildInputs = emacs27.buildInputs
+                          ++ [ autoconf automake texinfo gcc libgccjit ]
+                          ++ lib.optionals stdenv.isDarwin (
+                            with darwin.apple_sdk.frameworks; [
+                              AppKit Carbon Cocoa GSS ImageIO ImageCaptureCore
+                              IOKit OSAKit Quartz QuartzCore WebKit
+                            ]);
+          });
     });
 }
