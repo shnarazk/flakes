85b87e4515b09063b1c1e3dbe911224586f7fccc
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-02-15 20:43:09 +0900

    add fukuoka-covid19

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     cadical.url           = github:shnarazk/flakes?dir=cadical;
     cargo-instruments.url = github:shnarazk/flakes?dir=cargo-instruments;
     emacs-head.url        = github:shnarazk/flakes?dir=emacs-head;
+    fukuoka-covid19.url   = github:shnarazk/learn-dioxus;
     gratchk.url           = github:shnarazk/flakes?dir=gratchk;
     gratgen.url           = github:shnarazk/flakes?dir=gratgen;
     sat-bench.url         = github:shnarazk/SAT-bench;
@@ -17,6 +18,7 @@
           cadical           = inputs.cadical.defaultPackage.${system};
           cargo-instruments = inputs.cargo-instruments.defaultPackage.${system};
           emacs-head        = inputs.emacs-head.defaultPackage.${system};
+          fukuoka-covid19   = inputs.fukuoka-covid19.defaultPackage.${system};
           gratchk           = inputs.gratchk.defaultPackage.${system};
           gratgen           = inputs.gratgen.defaultPackage.${system};
           sat-bench         = inputs.sat-bench.defaultPackage.${system};
