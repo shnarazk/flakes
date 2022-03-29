d9bcaf6394a5283d23432d7b44be1a071a3f575d
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-03-29 09:55:17 +0900

    ditto

--- flake.nix
+++ flake.nix
@@ -12,23 +12,26 @@
     splr.url              = github:shnarazk/splr;
   };
   outputs = { self, flake-utils, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem (system:
-      {
-        packages = {
-          cadical           = inputs.cadical.${system}.default;
-          emacs-head        = inputs.emacs-head.${system}.default;
-          fukuoka-c19       = inputs.fukuoka-c19.${system}.default;
-          # gratgen           = inputs.gratgen.${system}.default;
-          sat-bench         = inputs.sat-bench.${system}.default;
-          splr              = inputs.splr.${system}.default;
-        };
-        # // (
-        #   if system == "x86_64-darwin" then { # || system == "aarch64-darwin"
-        #     cargo-instruments = inputs.cargo-instruments.${system}.default;
-        #     gratchk           = inputs.gratchk.${system}.default;
-        #   } else {
-        #   });
-      }
-    );
+    flake-utils.lib.eachSystem
+      [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ]
+      (system:
+        {
+          packages = {
+            cadical           = inputs.cadical.packages.${system}.default;
+            emacs-head        = inputs.emacs-head.packages.${system}.default;
+            # fukuoka-c19       = inputs.fukuoka-c19.packages.${system}.default;
+            # gratgen           = inputs.gratgen.packages.${system}.default;
+            sat-bench         = inputs.sat-bench.packages.${system}.default;
+            # splr              = inputs.splr.packages.${system}.default;
+          };
+          # // (
+          #   if system == "x86_64-darwin" then { # || system == "aarch64-darwin"
+          #     cargo-instruments = inputs.cargo-instruments.${system}.default;
+          #     gratchk           = inputs.gratchk.${system}.default;
+          #   } else {
+          #   });
+        }
+      )
+    ;
 }
  
