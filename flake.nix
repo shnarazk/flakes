2b8c24040e99ddc9efa47004e308be56626c8f2d
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-03-29 09:44:44 +0900

    Update flake

--- flake.nix
+++ flake.nix
@@ -18,16 +18,16 @@
           cadical           = inputs.cadical.${system}.default;
           emacs-head        = inputs.emacs-head.${system}.default;
           fukuoka-c19       = inputs.fukuoka-c19.${system}.default;
-          gratgen           = inputs.gratgen.${system}.default;
+          # gratgen           = inputs.gratgen.${system}.default;
           sat-bench         = inputs.sat-bench.${system}.default;
           splr              = inputs.splr.${system}.default;
-        }
-        // (
-          if system == "x86_64-darwin" then { # || system == "aarch64-darwin"
-            cargo-instruments = inputs.cargo-instruments.${system}.default;
-            gratchk           = inputs.gratchk.${system}.default;
-          } else {
-          });
+        };
+        # // (
+        #   if system == "x86_64-darwin" then { # || system == "aarch64-darwin"
+        #     cargo-instruments = inputs.cargo-instruments.${system}.default;
+        #     gratchk           = inputs.gratchk.${system}.default;
+        #   } else {
+        #   });
       }
     );
 }
