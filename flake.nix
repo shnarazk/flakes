1c587c407d68b171161d0fd3fd333cde9e6fbb34
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-03-29 10:12:44 +0900

    ditto

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     gratchk.url           = github:shnarazk/flakes?dir=gratchk;
     gratgen.url           = github:shnarazk/flakes?dir=gratgen;
     sat-bench.url         = github:shnarazk/SAT-bench;
-    splr.url              = github:shnarazk/splr;
+    splr.url              = github:shnarazk/splr?ref=exp-penetration-enegry-20220321;
   };
   outputs = { self, flake-utils, ... }@inputs:
     flake-utils.lib.eachSystem
@@ -22,7 +22,7 @@
             # fukuoka-c19       = inputs.fukuoka-c19.packages.${system}.default;
             # gratgen           = inputs.gratgen.packages.${system}.default;
             sat-bench         = inputs.sat-bench.packages.${system}.default;
-            # splr              = inputs.splr.packages.${system}.default;
+            splr              = inputs.splr.packages.${system}.default;
           };
           # // (
           #   if system == "x86_64-darwin" then { # || system == "aarch64-darwin"
