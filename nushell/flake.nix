c94ec333e6a96c5ad53f005be6be44fce2ec1bec
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-04-16 13:37:28 +0900

    modified:   flake.nix

--- nushell/flake.nix
+++ nushell/flake.nix
@@ -11,9 +11,16 @@
               name = system;
               value = {
                 default = nushell.overrideAttrs (attrs: rec {
-                    version = "0.61.0-20220416-1";
-                    rev = "3783c19d02f4a3b2f35cd26b79daabcb70b04880";
-                  });
+                    pname = "nushell";
+                    version = "0.61.0";
+                    src = fetchFromGitHub {
+                        owner = pname;
+                        repo = pname;
+                        rev = version;
+                        sha256 = "1qfqn22qabm0jrr4yqq9rb29k8qj9w9g0j9x4n8h0zp28vn7c2bq";
+                    };
+                    cargoSha256 = "sha256-gZ91rRyp5a7MjG9yM0pGCBYtM4GylZg7Sg9wCiB+SW0=";
+                });
               };
             })
           [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ]
