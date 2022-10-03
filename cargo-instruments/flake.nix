5e6899005c7758f7845f6d220c961f755ea6eb42
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-10-03 11:55:10 +0900

    modified:   cargo-instruments/flake.nix

--- cargo-instruments/flake.nix
+++ cargo-instruments/flake.nix
@@ -20,7 +20,7 @@
                   # nix flake prefetch https://github.com/cmyr/cargo-instruments/archive/${rev}.tar.gz
                   sha256 = "sha256-y06j1rJ1TUtZvlMNQWCeRie73sTnoABTXeFnJzALibg=";
                 };
-                buildInputs = rustc.buildInputs ++ [ cargo rustc pkgconfig ];
+                buildInputs = rustc.buildInputs ++ [ cargo rustc pkg-config ];
                 buildPhase = "cargo build --release";
                 installPhase = "mkdir -p $out/bin; install -t $out/bin target/release/cargo-instruments";
               };
