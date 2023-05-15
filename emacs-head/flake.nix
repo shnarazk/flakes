97c6a213e4307e7621b8f379f7a94b1eede1e5ed
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-05-16 07:31:11 +0900

    modified:   emacs-head/flake.nix

--- emacs-head/flake.nix
+++ emacs-head/flake.nix
@@ -18,12 +18,12 @@
             value = {
               default = emacs.overrideAttrs (attrs: rec {
                   pname = "emacs-head";
-                  version = "29.0.90-1";
+                  version = "29.0.91-1";
                   name = "emacs-head-${version}";
-                  rev = "a44d906740f0d8b2bf11b4db4f1cce88f4382692";
+                  # rev = "a44d906740f0d8b2bf11b4db4f1cce88f4382692";
                   src = fetchurl {
-                    url = "https://alpha.gnu.org/gnu/emacs/pretest/emacs-29.0.90.tar.xz";
-                    sha256 = "sha256-oUf6XX09Yh0k1V+lY9vx9M6UBUW1x00TiuANDDs1ZrY=";
+                    url = "https://alpha.gnu.org/gnu/emacs/pretest/emacs-29.0.91.tar.xz";
+                    sha256 = "sha256-7HdxT9qGlCpLg1LDbCM68vIn10u3FnP/2Avpqu+pHLI=";
                   };
                 }
               );
