b443e720a2c7094adafd92ec4824af4b77461eb6
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-08-31 18:35:16 +0900

    modified:   flake.nix

--- zellij-head/flake.nix
+++ zellij-head/flake.nix
@@ -10,18 +10,18 @@
             value = {
               default = zellij.overrideAttrs (attrs: rec {
                 pname = "zellij-head";
-                version = "0.38.0";
+                version = "0.38.1";
                 name = "${pname}-${version}";
                 src = fetchFromGitHub {
                   owner = "zellij-org";
                   repo = "zellij";
                   rev = "refs/tags/v${version}";
-                  sha256 = "sha256-+i4lY2VB/g6/dwN4dDwsiI+3R299ssBY90fpnvkVrXU=";
+                  sha256 = "sha256-3khMo5qMG0qonMnPvuErVYFohUrZLAkaxKZzkMHou8E=";
                 };
                 cargoDeps = zellij.cargoDeps.overrideAttrs (lib.const {
                   name = "${pname}-vendor.tar.gz";
                   inherit src;
-                  outputHash = "sha256-ybDErEizthAOq2IyehQNL4tMJHtaE3Ejw6janTFWcyc=";
+                  outputHash = "sha256-kjEbmyB2atvT+MsBQOWYhKwi7428e48gSMGxPwY6/RY=";
                 });
               });
             };
