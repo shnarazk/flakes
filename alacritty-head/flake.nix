4cc08b6c69bfa5c355bddf2b52fd4a59871f735e
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-12-14 17:11:39 +0900

    modified:   alacritty-head/flake.nix

--- alacritty-head/flake.nix
+++ alacritty-head/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Alacritty overlay to Head, the released 0.12.3";
+  description = "Alacritty overlay to Head, the released 0.13.0";
   inputs.nixpkgs.url = github:NixOS/nixpkgs;
   outputs = { self, nixpkgs }: {
     packages = builtins.listToAttrs
@@ -10,20 +10,56 @@
             value = {
               default = alacritty.overrideAttrs (attrs: rec {
                 pname = "alacritty-head";
-                version = "0.12.3";
+                version = "0.13.0-rc1";
                 name = "${pname}-${version}";
                 src = fetchFromGitHub {
                   owner = "alacritty";
                   repo = "alacritty";
                   rev = "refs/tags/v${version}";
-                  sha256 = "sha256-SUEI7DTgs6NYT4oiqaMBNCQ8gP1XoZjPFIKhob7tfsk=";
+                  sha256 = "sha256-5PK8poco+HXbd6zuSjr9of2qSgC8puSTSzZnI4vvXEI=";
                 };
                 # https://discourse.nixos.org/t/is-it-possible-to-override-cargosha256-in-buildrustpackage/4393
                 cargoDeps = alacritty.cargoDeps.overrideAttrs (lib.const {
                   name = "${pname}-vendor.tar.gz";
                   inherit src;
-                  outputHash = "sha256-aXndUdMHMitihramSifYwm7LNL6TWLyVVlPxoxyc8K8=";
+                  outputHash = "sha256-EaJaSGqc1vQURdkZLs2h/6You33wDmHNgg+BaHlvVs8=";
                 });
+                outputs = [ "out" ];
+                postInstall = (
+                    if stdenv.isDarwin then ''
+                      mkdir $out/Applications
+                      cp -r extra/osx/Alacritty.app $out/Applications
+                      ln -s $out/bin $out/Applications/Alacritty.app/Contents/MacOS
+                    '' else ''
+                      install -D extra/linux/Alacritty.desktop -t $out/share/applications/
+                      install -D extra/linux/org.alacritty.Alacritty.appdata.xml -t $out/share/appdata/
+                      install -D extra/logo/compat/alacritty-term.svg $out/share/icons/hicolor/scalable/apps/Alacritty.svg
+
+                      # patchelf generates an ELF that binutils' "strip" doesn't like:
+                      #    strip: not enough room for program headers, try linking with -N
+                      # As a workaround, strip manually before running patchelf.
+                      $STRIP -S $out/bin/alacritty
+
+                      patchelf --add-rpath "${lib.makeLibraryPath rpathLibs}" $out/bin/alacritty
+                    ''
+                  ) + ''
+
+                    installShellCompletion --zsh extra/completions/_alacritty
+                    installShellCompletion --bash extra/completions/alacritty.bash
+                    installShellCompletion --fish extra/completions/alacritty.fish
+
+                    # install -dm 755 "$out/share/man/man1"
+                    # gzip -c extra/alacritty.man > "$out/share/man/man1/alacritty.1.gz"
+                    # gzip -c extra/alacritty-msg.man > "$out/share/man/man1/alacritty-msg.1.gz"
+
+                    # install -Dm 644 alacritty.yml $out/share/doc/alacritty.yml
+
+                    # install -dm 755 "$terminfo/share/terminfo/a/"
+                    # tic -xe alacritty,alacritty-direct -o "$terminfo/share/terminfo" extra/alacritty.info
+                    # mkdir -p $out/nix-support
+                    # echo "$terminfo" >> $out/nix-support/propagated-user-env-packages
+                  '';
+
               });
             };
           }
