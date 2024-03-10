7e9268c69cb09d2dae036ce815050643bc4f4efa
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-03-10 14:49:54 +0900

    modified:   rust-parallel/flake.nix

--- rust-parallel/flake.nix
+++ rust-parallel/flake.nix
@@ -10,20 +10,20 @@
             value = {
               default = rustPlatform.buildRustPackage rec {
                 pname = "rust-parallel";
-                version = "1.16.1";
+                version = "1.17.0";
 
                 src = fetchFromGitHub {
                   owner = "aaronriekenberg";
                   repo = pname;
                   rev = "refs/tags/v${version}";
-                  hash = "sha256-z+u0eX5XfjdaqFt9Uszm3q/3Y3CkQUBL/M6zfin5g5w=";
+                  hash = "sha256-1eRBQoUY7IKlmoIqUmmsRH/2XdqfuRFzLW9xBDWALzU=";
                 };
 
                 nativeBuildInputs = [ openssl pkg-config libgit2 ];
                 buildInputs = [ openssl libgit2 ] ++ lib.optional stdenv.isDarwin [iconv];
                 PKG_CONFIG_PATH = "${openssl.dev}/lib/pkgconfig";
                 doCheck = false;
-                cargoHash = "sha256-/emTrbt6mId/NPp67NWqIkUWC4WMgoqD89aa9t6MjDE=";
+                cargoHash = "sha256-cY2dosS+zeZgiTPbvCGYp7BDVlUIFlH/P+CJxDM//zU=";
 
                 meta = with lib; {
                   description = "Fast command line app in rust/tokio to run commands in parallel. Similar interface to GNU parallel or xargs plus useful features. Listed in Awesome Rust utilities.";
