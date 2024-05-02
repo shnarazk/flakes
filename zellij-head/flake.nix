78b515a185cd5631529b7a1f2555a0371611b2ae
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-05-02 20:04:14 +0900

    modified:   zellij-head/flake.nix

--- zellij-head/flake.nix
+++ zellij-head/flake.nix
@@ -10,20 +10,20 @@
             value = {
               default = zellij.overrideAttrs (attrs: rec {
                 pname = "zellij-head";
-                version = "0.40.0";
+                version = "0.40.1";
                 name = "${pname}-${version}";
                 src = fetchFromGitHub {
                   owner = "zellij-org";
                   repo = "zellij";
                   rev = "refs/tags/v${version}";
-                  sha256 = "sha256-1z7qIgjElE9gw0lY+A17gsYIcnKRceZTFnRd8fMeBg8=";
+                  sha256 = "sha256-n8cwsCeKWzTw/psvLL3chBr8EcwGoeKB8JeiLSLna1k=";
                 };
                 buildInputs = zellij.buildInputs ++ [ openssh pkg-config perl ];
                 nativeBuildInputs = zellij.nativeBuildInputs ++ [ openssh pkg-config perl ];
                 cargoDeps = zellij.cargoDeps.overrideAttrs (lib.const {
                   name = "${pname}-vendor.tar.gz";
                   inherit src;
-                  outputHash = "sha256-1FfZ7AgZKGhpMf6HKB91CFUXP49B2/WaS2T94i7R8q8=";
+                  outputHash = "sha256-7NZZ5PLOhONVokGnWUfGoxT3aTWeugcTfEcEc1Go94I=";
                 });
               });
             };
