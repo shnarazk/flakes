ae0e8d41ce24d68dcc28848bff4f75f16303096f
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-08-24 16:48:37 +0900

    new file:   monaspace/flake.nix

---
+++ monaspace/flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Emacs Head, the unreleased 29.1";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  outputs =
+    { self, nixpkgs }:
+    {
+      packages = builtins.listToAttrs (
+        map
+          (
+            system: with import nixpkgs { inherit system; }; {
+              name = system;
+              value = {
+                default = monaspace.overrideAttrs (attrs: rec {
+                  version = "1.300";
+                  name = "monaspace-${version}";
+                  src = fetchzip {
+                    url = "https://github.com/githubnext/monaspace/releases/download/v${version}/monaspace-static-v${version}.zip";
+                    stripRoot = false;
+                    hash = "sha256-2xHAB8+3lDiRlZPKy5SLZzyThbFHaS4HNpQVzE/dQZQ=";
+                  };
+                  outputs = [ "out" ];
+
+                  installPhase = ''
+                    runHook preInstall
+                    install -Dm644 Static\ Fonts/*/*.otf -t $out/share/fonts/opentype
+                    runHook postInstall
+                  '';
+                });
+              };
+            }
+          )
+          [
+            "x86_64-linux"
+            "aarch64-linux"
+            "x86_64-darwin"
+            "aarch64-darwin"
+          ]
+      );
+    };
+}
