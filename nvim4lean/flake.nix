25c4ee8026c4bc4cf22cdea7868e5acfeda337e6
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-11-28 12:42:26 +0900

    modified:   nvim4lean/flake.nix

--- nvim4lean/flake.nix
+++ nvim4lean/flake.nix
@@ -17,6 +17,7 @@
                   vim.cmd('cabbrev bc bd')
                   vim.cmd('cabbrev o edit')
                   vim.cmd('set grepprg=rg\\ --vimgrep')
+                  vim.lsp.config("leanls", { init_options = { editDelay = 100; } });
                 '';
                 globals = {
                   mapleader = " ";
@@ -168,9 +169,6 @@
                 plugins.lean.enable = true;
                 plugins.lean.autoLoad = true;
                 plugins.lean.settings.mappings = true;
-                plugins.lean.settings.lsp.init_options = {
-                  editDelay = 50;
-                };
                 plugins.lean.settings.progress_bars.enable = false;
                 plugins.lualine = {
             		  enable = true;
